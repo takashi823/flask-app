63ab1318d2b254d953feb4684390cd0cfecf0349
takashi823 <uchimura.takashi724@gmail.com>
2023-11-22 12:41:16 +0900

    add: サンプルの画面追加

--- app/app.py
+++ app/app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+	return render_template("signup.html")
+
+@app.route("/post")
+def post():
+	return render_template("post.html")
+
+@app.route("/postlist")
+def postList():
+	return render_template("postList.html")
+
+
 if __name__ == '__main__':
 	app.run()
