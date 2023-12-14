d78e40435533df4788ec40a0c1bd419aec3b9c04
takashi823 <uchimura.takashi724@gmail.com>
2023-12-15 08:51:06 +0900

    update:DBの設定追加

--- app/app.py
+++ app/app.py
@@ -1,60 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
-# importの追加
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-
-# DBの接続設定 
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8".format(
-    **{"user": "test", "password": "test", "host": "db", "db_name": "test"}
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# DBインスタンス作成
-db = SQLAlchemy(app)
-
-# migrateインスタンスを定義
-migrate = Migrate(app, db)
-
-# モデルの作成
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+from .models.database import init_db, db
+from .models.post import Post
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    init_db(app)
+
+    @app.route("/", methods=["GET"])
+    def index():
+        posts = Post.query.all()
+        print(posts)
+        return render_template("index.html", posts=posts)
+
+    @app.route("/post", methods=["GET", "POST"])
+    def add_task():
+        if request.method == "POST":
+            postInfo = Post(
+                title=request.form["title"],
+                content=request.form["content"],
+            )
+            print(postInfo)
+            db.session.add(postInfo)
+            db.session.commit()
+            return redirect("/")
+        else:
+            return render_template("post.html")
+
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    @app.route("/signup")
+    def signup():
+        return render_template("signup.html")
 
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
+    @app.route("/postlist")
+    def postList():
+        return render_template("postList.html")
 
+    return app
 
-@app.route("/post")
-def post():
-    return render_template("post.html")
-
-
-@app.route("/postlist")
-def postList():
-    return render_template("postList.html")
-
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
